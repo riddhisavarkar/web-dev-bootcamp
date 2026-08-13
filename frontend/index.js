6b76bb69fba3c552370e9597c0bbd2ce68a86b77
Riddhi <riddhisavarkar@gmail.com>
2026-08-13 16:29:17 +0530

    Added toggle

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,23 @@
-alert("welcome to web bootcamp")
+//alert("welcome to web bootcamp")
+var users =[
+    {
+        "name" : "John Doe",
+        "gender" : "Male",
+        "image" : "john.png"
+    },
+    {
+        "name" : "Jane Smith",
+        "gender" : "Female",
+        "image" : "jane.png"
+    }
+]
+var curId = 0;
+function toggleUser(){
+    curId = (curId + 1) % 2;
+    var username = document.getElementById("user-name");
+    var usergender = document.getElementById("user-gender");
+    var userimage = document.getElementById("user-image");
+    username.innerHTML = users[curId].name;
+    usergender.innerHTML = users[curId].gender;
+    userimage.src = users[curId].image;
+}
