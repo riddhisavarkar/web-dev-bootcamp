995696804825b59b9991c406594aa35a34171ed6
Riddhi <riddhisavarkar@gmail.com>
2026-08-14 10:53:04 +0530

    added random user

--- frontend/index.js
+++ frontend/index.js
@@ -13,11 +13,33 @@ var users =[
 ]
 var curId = 0;
 function toggleUser(){
-    curId = (curId + 1) % 2;
+    curId = (curId + 1) % users.length;
     var username = document.getElementById("user-name");
     var usergender = document.getElementById("user-gender");
     var userimage = document.getElementById("user-image");
     username.innerHTML = users[curId].name;
     usergender.innerHTML = users[curId].gender;
     userimage.src = users[curId].image;
+}
+function randomUser() {
+    fetch("https://randomuser.me/api")
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      var userName = document.getElementById("user-name");
+      var userGender = document.getElementById("user-gender");
+      var userImage = document.getElementById("user-image");
+
+      var newUserName = data.results[0].name.first + " " + data.results[0].name.last;
+      var newUserGender = data.results[0].gender;
+      var newUserImage = data.results[0].picture.large;
+      userName.innerHTML = newUserName;
+      userGender.innerHTML = newUserGender;
+      userImage.src = newUserImage;
+
+    })
+    .catch(function(err) {
+      console.log("Error occured : " + err);
+    })
 }
